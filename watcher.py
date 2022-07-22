47b913ac2cc026c58866d66b17ded8a0b52b6cbe
Nathan Gavenski <nathangavenski@gmail.com>
2022-07-22 09:55:37 -0300

    Convert to argparse, add delta time for modify queue

--- watcher.py
+++ watcher.py
@@ -1,31 +1,39 @@
-import sys, logging, time, os
+import argparse
+import time, os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 class MyHandler(FileSystemEventHandler):
     def __init__(self, params):
         self.params = params
+        self.last_trigger = time.time()
 
     def on_modified(self, event):
-        print('event type: {type}  path : {path}'.format(type=event.event_type, path=event.src_path))
-        os.system('clear')
-        print('Running {file}...'.format(file=params['-f']))
-        os.system('python {path}/{file}'.format(path=params['-d'], file=params['-f']))
+        delta = time.time() - self.last_trigger
+        if 'swp' not in event.src_path and delta > self.params.time:
+            os.system('clear')
+            print(f'Event type: {str(event.event_type).upper()}\nPath : {event.src_path}')
+            print(f'Running {self.params.file}...')
+            os.system(f'python {self.params.dir}/{self.params.file}')
+            self.last_trigger = time.time() 
         
 
-if __name__ == "__main__":
-    params = {}
+def get_args() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description="Args for watching over a file")
+    parser.add_argument('-d', '--dir', type=str, help='Dir the file is located')
+    parser.add_argument('-f', '--file', type=str, help='File name')
+    parser.add_argument('-t', '--time', type=int, default=1, help='Time delta between saves (save processing if constant saving')
+    return parser.parse_args()
 
-    for index, value in enumerate(sys.argv):
-        if  index % 2 > 0 and value[0] == '-':
-            params[value] = sys.argv[index + 1]
+
+if __name__ == "__main__":
+    params = get_args()
 
     os.system('clear')
-    print('Watching over {type}/{file}'.format(type=params['-d'], file=params['-f']))
-    path = params['-d']
+    print(f'Watching over {params.dir}/{params.file}')
     event_handler = MyHandler(params)
     observer = Observer()
-    observer.schedule(event_handler, path=params['-d'], recursive=True)
+    observer.schedule(event_handler, path=params.dir, recursive=True)
     observer.start()
 
     try:
